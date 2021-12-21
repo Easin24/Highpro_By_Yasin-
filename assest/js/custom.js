c069bdff16bc50983027a065c9030830ed1be447
Easin24 <86684883+Easin24@users.noreply.github.com>
2021-12-21 17:11:45 +0600

    Add files via upload

---
+++ assest/js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(
+    function(){
+        $('.counter').counterUp({
+            delay: 50,
+            time: 1000,
+        });
+        $('body').materialScrollTop();
+    }
+);
